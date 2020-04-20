9a80050eeb7881671be4742bb33020e5d674b8a6
Li Luo <cluo.cassie@gmail.com>
2020-04-20 16:19:02 +0000

    fix test

--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -2,6 +2,6 @@ import org.scalatest._
 
 class HelloSpec extends FunSuite with DiagrammedAssertions {
   test("Hello should start with H") {
-    assert("hello".startsWith("H"))
+    assert("hello".startsWith("h"))
   }
 }
